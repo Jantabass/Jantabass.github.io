63214491bd561064bc7deef7511fdc16b25a470a
Jonathan <jonamic2510@gmail.com>
2026-01-21 07:56:47 +0100

    gjort startsiden penere

--- script.js
+++ script.js
@@ -436,3 +436,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+setInterval(() => {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}, 5000);
